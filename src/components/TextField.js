a9fde64f2f2e11cf61b3202f6fa91f03a850c75b
ab85hishek <ab85hishek@gmail.com>
2022-11-18 14:48:51 +0530

    updated

--- src/components/TextField.js
+++ src/components/TextField.js
@@ -1,39 +1,59 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 function TextField() {
-
     const [text, setText] = useState("");
 
-    function changeHandler(e){
+    function changeHandler(e) {
         console.log("change");
         setText(e.target.value);
     }
 
-    function clickUCHandler(){
+    function clickUCHandler() {
         let res = text;
-        setText(res.toUpperCase())
+        setText(res.toUpperCase());
+    }
+    function clickLCHandler() {
+        let res = text;
+        setText(res.toLowerCase());
     }
 
-    function clickClearHandler(){
+    function clickClearHandler() {
         setText("");
     }
 
-  return (
-    <>
-      <div className="form-group">
-      <h1>Enter your text below:</h1>
-        <textarea
-          className="form-control"
-          id="exampleFormControlTextarea1"
-          value={text}
-          onChange = {changeHandler} 
-          rows="5"
-        ></textarea>
-        <button className="btn btn-primary my-3" onClick={clickUCHandler}>Convert to Upper Case</button>
-        <button className="btn btn-primary my-3 mx-3" onClick={clickClearHandler}>Clear</button>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="form-group">
+                <h1>Enter your text below:</h1>
+                <textarea
+                    className="form-control"
+                    id="exampleFormControlTextarea1"
+                    value={text}
+                    onChange={changeHandler}
+                    rows="5"
+                ></textarea>
+                <button className="btn btn-primary my-3" onClick={clickUCHandler}>
+                    Convert to Upper Case
+                </button>
+                <button className="btn btn-primary my-3 mx-3" onClick={clickLCHandler}>
+                    Convert to Lower Case
+                </button>
+                <button
+                    className="btn btn-primary my-3 "
+                    onClick={clickClearHandler}
+                >
+                    Clear
+                </button>
+            </div>
+            <div className="container">
+                <h3>Summary of your input: </h3>
+                <li>Word count : {text.split(" ").length}</li>
+                <li>Character count : {text.length}</li>
+                <h3 className="my-3">Preview</h3>
+                <p align="justify">{text}</p>
+            </div>
+        </>
+    );
 }
 
 export default TextField;
