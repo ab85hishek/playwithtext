e452a9cdb2c76bd2bac458aad8afa5699430cd5b
ab85hishek <ab85hishek@gmail.com>
2022-11-21 14:05:00 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -1,52 +1,67 @@
-import React,{ useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Alert from "./components/Alert";
 import Navbar from "./components/Navbar";
 import TextField from "./components/TextField";
+import About from "./components/About";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes,
+} from "react-router-dom";
 
 function App() {
-
-  const [mode,setMode] = useState('light');
+  const [mode, setMode] = useState("light");
   const [alert, setAlert] = useState(null);
   const [theme, setTheme] = useState(mode);
 
-  function toggleMode(){
-    if(mode==='light'){
-      setMode('dark');
-      document.body.style.backgroundColor = '#1b1a4c'
-      showAlert("Dark mode enabled", "Success!")
-    }else{
-      setMode('light');
-      document.body.style.backgroundColor = 'white'
-      showAlert("Light mode enabled", "Success!")
-
+  function toggleMode() {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#1b1a4c";
+      showAlert("Dark mode enabled", "Success!");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode enabled", "Success!");
     }
   }
 
-  function showAlert(message, type){
+  function showAlert(message, type) {
     setAlert({
       text: message,
-      type: type
-    })
-    setTimeout(()=>{
-      setAlert(null)
-    },1000)
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1000);
   }
 
-  function showTheme(value){
+  function showTheme(value) {
     setTheme(value);
     document.body.style.backgroundColor = theme;
   }
 
-
-  return <>
-    <Navbar title = "PlayWithText" mode= {mode} toggleMode= {toggleMode} showTheme = {showTheme}></Navbar>
-    <Alert alert = {alert}/>
-    <div className="container">
-    <TextField mode={mode} showAlert={showAlert}/>
-    </div>
-    
-  </>;
+  return (
+    <>
+      <Router>
+        <Navbar
+          title="PlayWithText"
+          mode={mode}
+          toggleMode={toggleMode}
+          showTheme={showTheme}
+        ></Navbar>
+        <Alert alert={alert} />
+        <div className="container">
+          <Routes>
+          <Route path="/" element={<TextField mode={mode} showAlert={showAlert} />}></Route>
+          <Route path="about" element={<About />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function About(){
+    return(
+        <>
+            <div className="container">
+                <h1>About Us</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, inventore cumque omnis perspiciatis quia ipsum temporibus ex explicabo officia aspernatur porro dolorum, aliquid libero, fugit dignissimos? Dicta itaque nam tempore?
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, inventore cumque omnis perspiciatis quia ipsum temporibus ex explicabo officia aspernatur porro dolorum, aliquid libero, fugit dignissimos? Dicta itaque nam tempore?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, inventore cumque omnis perspiciatis quia ipsum temporibus ex explicabo officia aspernatur porro dolorum, aliquid libero, fugit dignissimos? Dicta itaque nam tempore?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, inventore cumque omnis perspiciatis quia ipsum temporibus ex explicabo officia aspernatur porro dolorum, aliquid libero, fugit dignissimos? Dicta itaque nam tempore?
+                </p>
+            </div>
+        </>
+    )
+}
+
+export default About;
